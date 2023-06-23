ef4bdcdf3fa5d13bc019a2dbfca677b09d32d4ea
Michael Rosenberg <drmsrosenberg@gmail.com>
2023-06-23 13:14:07 -0600

    update phylogeny figure
    Phylogeny figure needs to be kept independent of normal graphical output as its displayed in a separate tab

--- src/MetaWinCharts.py
+++ src/MetaWinCharts.py
@@ -1155,7 +1155,10 @@ def draw_tree(faxes, tree, minx, maxx, miny, maxy, scale, draw_labels: bool = Fa
 
 def chart_phylogeny(root) -> FigureCanvasQTAgg:
     # set up drawing space
-    figure_canvas, faxes = base_figure()
+    figure_canvas =  FigureCanvasQTAgg(Figure(figsize=(8, 6)))
+    faxes = figure_canvas.figure.subplots()
+    faxes.spines["right"].set_visible(False)
+    faxes.spines["top"].set_visible(False)
     faxes.spines["bottom"].set_visible(False)
     faxes.spines["left"].set_visible(False)
     faxes.get_xaxis().set_visible(False)
